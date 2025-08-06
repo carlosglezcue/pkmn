792d8499d56e5f78a3f944a280303fbeee5fabb0
Carlos González Cuesta <carlos.gonzalezc@babelgroup.com>
2025-08-06 20:15:15 +0200

    Initial Commit

---
+++ PokePokeMON/PokePokeMONApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokePokeMONApp.swift
+//  PokePokeMON
+//
+//  Created by carlos.gonzalezc on 6/8/25.
+//
+
+import SwiftUI
+
+@main
+struct PokePokeMONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokePokeMONTests/PokePokeMONTests.swift
@@ -0,0 +1,17 @@
+//
+//  PokePokeMONTests.swift
+//  PokePokeMONTests
+//
+//  Created by carlos.gonzalezc on 6/8/25.
+//
+
+import Testing
+@testable import PokePokeMON
+
+struct PokePokeMONTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
