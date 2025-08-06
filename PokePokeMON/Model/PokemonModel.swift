54246f71d751eb932a7fb3991b6d8c4820a06bfa
Carlos González Cuesta <carlos.gonzalezc@babelgroup.com>
2025-08-06 21:04:25 +0200

    Project configuration & architecture
    WHAT:
    - Add folders
    - Add app icon
    - Add some necessary files
    - Add string catalog

---
+++ PokePokeMON/Model/PokemonModel.swift
@@ -0,0 +1,12 @@
+//
+//  PokemonModel.swift
+//  PokePokeMON
+//
+//  Created by carlos.gonzalezc on 6/8/25.
+//
+
+import Foundation
+
+struct PokemonModel {
+    
+}
---
+++ PokePokeMON/View/PokeDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  PokeDetailView.swift
+//  PokePokeMON
+//
+//  Created by carlos.gonzalezc on 6/8/25.
+//
+
+import SwiftUI
+
+struct PokeDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PokeDetailView()
+}
